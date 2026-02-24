4fc3fbdffc8d626a61bc38248e3415dc59279371
marylou014 <marylou.dumas@ecoles-epsi.net>
2026-02-24 17:21:23 +0100

    veille + contacte + correction

--- script.js
+++ script.js
@@ -15,25 +15,11 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// Fonction pop-up 
-function loadAndOpenModal(title, filePath) {
-    // 1. On va chercher le contenu du fichier HTML
-    fetch(filePath)
-        .then(response => {
-            if (!response.ok) throw new Error("Fichier introuvable");
-            return response.text();
-        })
-        .then(htmlContent => {
-            // 2. On remplit la modale avec le titre et le contenu récupéré
-            document.getElementById('modalTitle').innerHTML = title;
-            document.getElementById('modalBody').innerHTML = htmlContent;
-            
-            // 3. On affiche la modale
-            document.getElementById('modal').style.display = "flex";
-        })
-        .catch(error => {
-            console.error("Erreur lors du chargement de la modale :", error);
-        });
+// Fonctions pour la modale
+function openModal(title, text) {
+    document.getElementById('modalTitle').innerText = title;
+    document.getElementById('modalBody').innerText = text;
+    document.getElementById('modal').style.display = "flex";
 }
 
 function closeModal() {
